26bc24b71c450e74491d391509e56de1415fd768
BarakG <barakger13@gmail.com>
2020-04-04 17:12:15 +0300

    code now closes the file

--- practice.py
+++ practice.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-def open_without_closing(filename):
-    fd1 = open(filename, 'a')
-    fd1.write('I wouldn\'t do with it anything\n')
+def open_with_closing(filename):
+    with open(filename, 'a') as fd1:
+        fd1.write('I wouldn\'t do with it anything\n')
 
 
 def main():
-    open_without_closing(filename)
-    fd2 = open(filename, 'r')
-    for line in fd2:
-        print line,
+    open_with_closing(filename)
+    with open(filename, 'r') as fd2:
+        for line in fd2:
+            print line,
 
 if __name__ == '__main__':
     filename = r'C:\Heights\PortableApps\PortablePython2.7.6.1\App\hafifot\python101\temp_file.txt'
