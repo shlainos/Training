073210140ef6b9a21dd1b822d50c71e064472122
BarakG <barakger13@gmail.com>
2020-04-04 17:01:06 +0300

    wrote the example code with example text file to see output

---
+++ practice.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+def open_without_closing(filename):
+    fd1 = open(filename, 'a')
+    fd1.write('line 2\n')
+
+
+def main():
+    open_without_closing(filename)
+    fd2 = open(filename, 'r')
+    for line in fd2:
+        print line,
+
+if __name__ == '__main__':
+    filename = r'C:\Heights\PortableApps\PortablePython2.7.6.1\App\hafifot\python101\temp_file.txt'
+    main()
