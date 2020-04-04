c4317eb0aef982367bee0f3cefb164c9b85ccdf7
BarakG <barakger13@gmail.com>
2020-04-04 17:36:57 +0300

    copied content of origin file to copy file

--- practice.py
+++ practice.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-def open_with_closing(filename):
-    with open(filename, 'a') as fd1:
-        fd1.write('I wouldn\'t do with it anything\n')
+def copy_file(Origin, Copy):
+    '''  copies the Origin file contents into the file Copy '''
+    Copy.write(Origin.read())
 
 
 def main():
-    open_with_closing(filename)
-    with open(filename, 'r') as fd2:
-        for line in fd2:
-            print line,
+    Origin_File = r'C:\Heights\PortableApps\PortablePython2.7.6.1\App\hafifot\python101\origin_file.txt'
+    Copy_File = r'C:\Heights\PortableApps\PortablePython2.7.6.1\App\hafifot\python101\copy_file.txt'
+    with open(Origin_File, 'r') as Origin:
+        with open(Copy_File, 'a') as Copy:
+            copy_file(Origin, Copy)
 
-if __name__ == '__main__':
-    filename = r'C:\Heights\PortableApps\PortablePython2.7.6.1\App\hafifot\python101\temp_file.txt'
-    main()
+
+main()
